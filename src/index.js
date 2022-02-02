8de3fec0a690f148583b7d9178397d7c127b7853
AlexxPo <96181892+AlexxPo@users.noreply.github.com>
2022-02-02 18:01:56 +0300

    Update index.js
    app.css => App.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
-import './app.css';
+import './App.css';
 
 import App from './containers/App';
 
